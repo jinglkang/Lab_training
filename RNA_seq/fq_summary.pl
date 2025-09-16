488085f53ffb6b79082ffeec4679ec50610a3822
Kang Jingliang <45533802+jinglkang@users.noreply.github.com>
2025-09-16 15:32:08 +0800

    Update fq_summary.pl

--- RNA_seq/fq_summary.pl
+++ RNA_seq/fq_summary.pl
@@ -6,17 +6,17 @@ my @txts=<$ARGV[0]/*fastqc/summary.txt>;
 die "There is no txt file\n" unless @txts;
 my %hash;
 foreach my $txt (@txts) {
-	open TXT, $txt or die "can not open $txt\n";
-	while (<TXT>) {
-		my @a=split /\t/;
-		if (/FAIL/i || /WARN/i) {
-			my $name=$a[0]."\t".$a[1];
-			$hash{$name}++;
-		}
-	}
+        open TXT, $txt or die "can not open $txt\n";
+        while (<TXT>) {
+                my @a=split /\t/;
+                if (/FAIL/i || /WARN/i) {
+                        my $name=$a[0]."\t".$a[1];
+                        $hash{$name}++;
+                }
+        }
 }
 
 foreach my $key (sort keys %hash) {
-	my $nub=$hash{$key};
-	print "$key\t$nub\n";
+        my $nub=$hash{$key};
+        print "$key\t$nub\n";
 }
